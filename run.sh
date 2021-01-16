8b5c50add5efcc31e427cdc7e422fb02fa94a3bd
liuzhen <liuzhenm@mail.ustc.edu.cn>
2021-01-16 10:29:48 +0800

    init upload

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/kvgroup/anaconda3/bin/python3 /home/kvgroup/zhenliu/pyproject/USTC-COVID19-Auto-Reporter/reporter.py >>/home/kvgroup/zhenliu/pyproject/USTC-COVID19-Auto-Reporter/report.log
---
+++ sendEmail.py
@@ -0,0 +1,31 @@
+#! /home/kvgroup/anaconda3/bin/python3
+# -*- coding: UTF-8 -*-
+ 
+import smtplib
+import sys
+from email.mime.text import MIMEText
+from email.header import Header
+
+class SendMessage:
+    def __init__(self ) -> None:
+        return
+    def send(self, to_msg :str, to_add :str, mail_user :str, mail_pass :str) ->bool:
+        mail_host = "mail.ustc.edu.cn"
+        mail_user = mail_user
+        mail_pass = mail_pass
+        sender = mail_user
+        receivers = [to_add]
+        conten = to_msg
+        message = MIMEText(conten, 'plain', 'utf-8')
+        message['From'] = sender
+        message['To'] = to_add
+        subject = 'COVID-19'
+        message['Subject'] = Header(subject, 'utf-8')
+        try:
+            smtpObj = smtplib.SMTP()
+            smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+            smtpObj.login(mail_user, mail_pass)
+            smtpObj.sendmail(sender, receivers, message.as_string())
+            return True
+        except smtplib.SMTPException as e:
+            return False
