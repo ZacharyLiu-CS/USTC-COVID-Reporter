56102b8fc528a352e81980f3d5f6f4d245c8f63b
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2021-08-18 09:19:22 +0800

    adjust for api change

--- reporter.py
+++ reporter.py
@@ -87,50 +87,52 @@ def main():
     if LOCATION == 1:
         report_payload = {
             '_token': token,                # 加入上面获得的token
-            'now_address' : '1',            # 当前所在地：内地
-            'gps_now_address': '',            #
-            'now_province': '340000',        # 当前所在地：安徽
-            'gps_province': '',                #
-            'now_city': '340100',            # 当前所在地：合肥
-            'gps_city': '',                    #
-            'now_detail': '',                #
-            'is_inschool': '6',                # 是否在校：西校区
-            'body_condition':    '1',        # 当前身体状况：正常
-            'body_condition_detail': '',    #
-            'now_status': '1',                # 当前状态：正常在校园内
-            'now_status_detail': '',        #
-            'has_fever': '0',                # 当前有无发热症状：无
-            'last_touch_sars': '0',            # 有无接触患者：无
-            'last_touch_sars_date': '',        #
-            'last_touch_sars_detail': '',    #
-            'last_touch_hubei': '0',        # 有无接触湖北人员：无
-            'last_touch_hubei_date': '',    #
-            'last_touch_hubei_detail': '',    #
-            'last_cross_hubei': '0',        # 有无在湖北停留或路过：无
-            'last_cross_sars_date': '',        #
-            'last_cross_sars_detail': '',    #
-            'return_dest': '1',                # 返校目的地：合肥校本部
-            'return_dest_detail': '',        #
-            'other_detail': '',                # 其他情况说明：（无）
+            'now_address': '1',
+            'gps_now_address': '',
+            'now_province': '340000',
+            'gps_province': '',
+            'now_city': '340100',
+            'gps_city': '',
+            'now_country': '340104',
+            'gps_country': '',
+            'now_detail': '',
+            'is_inschool': '6',
+            'body_condition': '1',
+            'body_condition_detail': '',
+            'now_status': '1',
+            'now_status_detail': '',
+            'has_fever': '0',
+            'last_touch_sars': '0',
+            'last_touch_sars_date': '',
+            'last_touch_sars_detail': '',
+            'is_danger': '0',
+            'is_goto_danger': '0',
+            'other_detail': ''
         }
     elif LOCATION == 2:
         report_payload = {
             '_token': token,                # 加入上面获得的token
-            'now_address' : '1',            # 当前所在地：内地
-            'gps_now_address': '',            #
-            'now_province': '320000',        # 当前所在地：江苏
-            'gps_province': '',                #
-            'now_city': '321200',            # 当前所在地：泰州
-            'gps_city': '',                    #
-            'now_detail': '',                #
-            'body_condition':    '1',        # 当前身体状况：正常
-            'body_condition_detail': '',    #
-            'now_status': '2',                # 当前状态：正常在校园内
-            'now_status_detail': '',        #
-            'has_fever': '0',                # 当前有无发热症状：无
-            'last_touch_sars': '0',            # 有无接触患者：无
-            'last_touch_sars_date': '',        #
-            'last_touch_sars_detail': '',    #
+
+            'now_address': '1',
+            'gps_now_address': '',
+            'now_province': '320000',  # 当前所在地: 江苏
+            'gps_province': '',
+            'now_city': '321200',      # 当前所在地: 泰州
+            'gps_city': '',
+            'now_country': '321282',   # 当前所在地: 靖江
+            'gps_country': '',
+            'now_detail': '',
+            'is_inschool': '6',
+            'body_condition': '1',
+            'body_condition_detail': '',
+            'now_status': '2',
+            'now_status_detail': '',
+            'has_fever': '0',
+            'last_touch_sars': '0',
+            'last_touch_sars_date': '',
+            'last_touch_sars_detail': '',
+            'is_danger': '0',
+            'is_goto_danger': '0',
             'other_detail': ''
             }
 
